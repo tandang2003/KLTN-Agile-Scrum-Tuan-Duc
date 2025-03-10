3bac31915349f8b252b9f36cb11be7af94784b77
Le Anh Duc <ducvui2003@gmail.com>
2025-03-10 10:25:04 +0700

    setup vite, tailwindcss, shadcn, react-router-dom

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import AppRoutes from '@/routes/AppRoutes'
+import { Providers } from '@/providers'
+
+function App() {
+  return (
+    <Providers>
+      <AppRoutes />
+    </Providers>
+  )
+}
+
+export default App
