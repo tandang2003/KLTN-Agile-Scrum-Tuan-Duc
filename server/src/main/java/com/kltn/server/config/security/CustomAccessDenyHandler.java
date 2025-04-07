6ea03d78f8fddb2aab7981035f06f1d703b95e58
tandang <21130171@st.hcmuaf.edu.vn>
2025-04-07 22:35:20 +0700

    finish security

--- server/src/main/java/com/kltn/server/config/security/CustomAccessDenyHandler.java
+++ server/src/main/java/com/kltn/server/config/security/CustomAccessDenyHandler.java
@@ -1,19 +1,32 @@
 package com.kltn.server.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kltn.server.DTO.response.ApiResponse;
+import com.kltn.server.config.security.exception.MyAuthenticationException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Optional;
+
 @Component
 public class CustomAccessDenyHandler implements AccessDeniedHandler {
+    @Autowired
+    ObjectMapper mapper;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-
+        response.setContentType("application/json;charset=UTF-8");
+        String errorMessage = Optional.ofNullable(accessDeniedException.getCause()).map(Throwable::getMessage).orElse(accessDeniedException.getMessage());
+        ApiResponse<Object> res = ApiResponse.builder().code(HttpStatus.FORBIDDEN.value()).error(errorMessage).message("User not have permission").build();
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        mapper.writeValue(response.getWriter(), res);
     }
 
 
--- server/src/main/java/com/kltn/server/config/security/CustomJWTDecoder.java
+++ server/src/main/java/com/kltn/server/config/security/CustomJWTDecoder.java
@@ -1,5 +1,7 @@
 package com.kltn.server.config.security;
 
+import com.kltn.server.config.security.exception.AuthenticationError;
+import com.kltn.server.config.security.exception.MyAuthenticationException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
@@ -17,12 +19,12 @@ public class CustomJWTDecoder implements JwtDecoder {
     }
 
     @Override
-    public Jwt decode(String token) throws JwtException {
+    public Jwt decode(String token) {
         Jwt jwt = defaultDecoder.decode(token);
         Instant ttl = jwt.getExpiresAt();
         if (ttl != null && !Instant.now().isAfter(ttl)) {
             return jwt;
         }
-        throw new JwtException("Invalid JWT");
+        throw new MyAuthenticationException(AuthenticationError.INVALID_CREDENTIALS);
     }
 }
