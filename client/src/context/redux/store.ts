62e49c8b5ff67ce5a5d8d06cb1e798ea1c3e4181
Le Anh Duc <ducvui2003@gmail.com>
2025-04-08 18:31:03 +0700

    feat: resolves#15 feat: combine HOC component to folder provider (router and redux)

---
+++ client/src/context/redux/store.ts
@@ -0,0 +1,18 @@
+import boardApi from '@/feature/board/board.api'
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+export const store = configureStore({
+  reducer: {
+    [boardApi.reducerPath]: boardApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(boardApi.middleware)
+})
+
+setupListeners(store.dispatch)
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
--- client/src/pages/manager/project/board/page.tsx
+++ client/src/pages/manager/project/board/page.tsx
@@ -1,27 +1,29 @@
 import Board from '@/components/board/Board'
-import { convert } from '@/lib/utils'
-import { BoardProps } from '@/components/board/type'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
-import { useEffect, useState } from 'react'
-import boardApi from '@/services/board.service'
-import { useParams } from 'react-router-dom'
+import { Skeleton } from '@/components/ui/skeleton'
+import { useGetBoardByProjectIdQuery } from '@/feature/board/board.api'
+import { convert } from '@/lib/utils'
+import { Id } from '@/types/other.type'
 import { ProjectParams } from '@/types/route.type'
+import { useParams } from 'react-router-dom'
 
 const BoardPage = () => {
   const { id } = useParams<ProjectParams>()
-  const [board, setBoard] = useState<BoardProps | null>(null)
-  useEffect(() => {
-    if (id)
-      boardApi.getData(id).then((response) => {
-        setBoard(convert(response))
-      })
-  }, [])
+  const { data, isFetching } = useGetBoardByProjectIdQuery(id as Id, {
+    skip: !id
+  })
 
   return (
-    <ScrollArea className='bg-[F9F8FF] whitespace-nowrap'>
-      {board && <Board {...board} />}
-      <ScrollBar orientation='horizontal' />
-    </ScrollArea>
+    <>
+      <span></span>
+      {isFetching && <Skeleton className={'h-4/5 rounded-xl bg-red-400'} />}
+      {!isFetching && data && (
+        <ScrollArea>
+          <Board {...convert(data)} />
+          <ScrollBar orientation='horizontal' />
+        </ScrollArea>
+      )}
+    </>
   )
 }
 
