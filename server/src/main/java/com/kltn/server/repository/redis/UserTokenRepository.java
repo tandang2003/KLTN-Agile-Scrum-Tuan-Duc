f0f885f7ec474cd917f2fe3e730ad098912ca88f
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-04-21 14:21:12 +0700

    config redis cache

--- server/src/main/java/com/kltn/server/repository/redis/UserTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kltn.server.repository.redis;
-
-import com.kltn.server.model.redis.UserToken;
-import org.springframework.data.repository.CrudRepository;
-public interface UserTokenRepository extends CrudRepository<UserToken, String> {
-
-
-}
