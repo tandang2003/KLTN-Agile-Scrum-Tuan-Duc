97ae694d8b2691312c3ac84075d42dc844d9eb27
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-04-26 13:50:19 +0700

    config redis for storeing and delete

--- server/src/main/java/com/kltn/server/service/redis/UserTokenService.java
+++ server/src/main/java/com/kltn/server/service/redis/UserTokenService.java
@@ -1,26 +1,48 @@
 package com.kltn.server.service.redis;
 
+import com.kltn.server.service.redis.config.RedisConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class UserTokenService {
-    RedisTemplate<String, String> redisTemplate;
+    RedisTemplate<String, String> refreshTemplate;
     @Value("${cache.redis.refresh-token.ttl}")
     private long refreshTokenTtl;
-
+    RedisTemplate<String, String> accessTemplate;
+    @Value("${cache.redis.access-token.ttl}")
+    private long accessTokenTtl;
 
     public UserTokenService(Map<String, RedisTemplate<?, ?>> redisTemplate) {
-        this.redisTemplate = (RedisTemplate<String, String>) redisTemplate.get("refreshToken");
+        this.refreshTemplate = (RedisTemplate<String, String>) redisTemplate.get(RedisConfig.REFRESH_TOKEN);
+        this.accessTemplate = (RedisTemplate<String, String>) redisTemplate.get(RedisConfig.ACCESS_TOKEN);
     }
 
-    public void saveToken(String key, String value) {
-        redisTemplate.opsForValue().set(key, value, refreshTokenTtl);
+    public void saveRefreshTokenExpired(String accessToken, String uniId) {
+        refreshTemplate.opsForValue().set(accessToken + uniId, accessToken, accessTokenTtl);
     }
 
-    public String getToken(String key) {
-        return redisTemplate.opsForValue().get(key);
+    public boolean isRefreshTokenExpired(String key) {
+        return refreshTemplate.hasKey(key);
+    }
+
+    public String getRefreshToken(String key) {
+        return refreshTemplate.opsForValue().get(key);
+    }
+
+    public void saveAccessTokenExpired(String accessToken, String uniId) {
+        accessTemplate.opsForValue().set(accessToken + uniId, accessToken, accessTokenTtl);
+    }
+
+    public boolean isAccessTokenExpired(String key) {
+        return accessTemplate.hasKey(key);
+    }
+
+    public String getAccessToken(String key) {
+        return accessTemplate.opsForValue().get(key);
     }
 }
