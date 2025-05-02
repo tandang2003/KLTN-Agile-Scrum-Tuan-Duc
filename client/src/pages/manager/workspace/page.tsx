84a9d424dc7294271fbd8e4a38573ab6c48f5626
Lê Anh Đức <leanhduc.develop@gmail.com>
2025-05-02 15:21:20 +0700

    Fix/get info reload page (#63)
    * feat: integrate with logout
    
    * feat: fix set token in local client
    
    * fix: UserDropdown
    
    * fix: add getter setter for UserDetailDTP in auth controller
    feat Testing integrate datatable shadcn with react table and rtk query
    
    * datatable
    
    * ref: DataTable split code for each feature
    feat: header info for workspace
    
    * fĩ: disable pointer event when dialog close
    
    * feat: form invite user to workspace
    
    * feat: move path variable to body invite user to workspace
    
    * fix: unchange old security config
    
    * fix: reset redux when logout
    fix: remove user/workspace

--- client/src/pages/manager/workspace/page.tsx
+++ client/src/pages/manager/workspace/page.tsx
@@ -4,6 +4,7 @@ import DialogCreateWorkspace from '@/components/dialog/DialogCreateWorkspace'
 import Icon from '@/components/Icon'
 import ListView from '@/components/ListView'
 import { Button } from '@/components/ui/button'
+import RequiredAuth from '@/components/wrapper/RequiredAuth'
 import { useAppDispatch, useAppSelector } from '@/context/redux/hook'
 import { RootState } from '@/context/redux/store'
 import { useGetListWorkspaceQuery } from '@/feature/workspace/workspace.api'
@@ -11,7 +12,10 @@ import { setStateDialogWorkspace } from '@/feature/workspace/workspace.slice'
 import { WorkspaceCardResponse } from '@/types/workspace.type'
 
 const WorkspacePage = () => {
-  const { data, isFetching } = useGetListWorkspaceQuery()
+  const { data, isFetching } = useGetListWorkspaceQuery({
+    page: 0,
+    size: 1
+  })
   const dispatch = useAppDispatch()
   const isDialogCreateOpen = useAppSelector(
     (state: RootState) => state.workspaceSlice.isDialogCreateOpen
@@ -21,12 +25,16 @@ const WorkspacePage = () => {
     <Container inSidebar>
       <div className='flex justify-between pt-2 pb-4'>
         <h2 className='h2'>Work Spaces</h2>
-        <Button
-          variant={'ghost'}
-          onClick={() => dispatch(setStateDialogWorkspace(!isDialogCreateOpen))}
-        >
-          <Icon icon={'lucide:plus'} />
-        </Button>
+        <RequiredAuth mode='hide' roles={['teacher']}>
+          <Button
+            variant={'ghost'}
+            onClick={() =>
+              dispatch(setStateDialogWorkspace(!isDialogCreateOpen))
+            }
+          >
+            <Icon icon={'lucide:plus'} />
+          </Button>
+        </RequiredAuth>
       </div>
       <ListView<WorkspaceCardResponse>
         data={data?.items}
