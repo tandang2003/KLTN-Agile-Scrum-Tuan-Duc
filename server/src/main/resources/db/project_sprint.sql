1f4e82bdccccc10260bab6ec78f8fcf2d9e38298
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-05-19 15:15:15 +0700

    fix logic issue, database, issue name properties

---
+++ server/src/main/resources/db/project_sprint.sql
@@ -0,0 +1,8 @@
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '0ddce180-7358-418d-9b9e-e621649236ef', '02731672-b887-4d1c-9e1a-e78333348aae');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '0ddce180-7358-418d-9b9e-e621649236ef', '5768c5fd-c556-4d1d-9c9a-5c3e89fa9876');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '0ddce180-7358-418d-9b9e-e621649236ef', 'a5c4f770-902b-4c37-af31-81a7593984ec');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '0ddce180-7358-418d-9b9e-e621649236ef', 'd3d0940c-9b5c-4a70-b75c-e8a622070a7c');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '955af4f8-7c20-4d6d-8e97-4084fc26417c', '02731672-b887-4d1c-9e1a-e78333348aae');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '955af4f8-7c20-4d6d-8e97-4084fc26417c', '5768c5fd-c556-4d1d-9c9a-5c3e89fa9876');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '955af4f8-7c20-4d6d-8e97-4084fc26417c', 'a5c4f770-902b-4c37-af31-81a7593984ec');
+INSERT INTO kltn.project_sprint (dt_planning, dt_preview, project_id, sprint_id) VALUES (null, null, '955af4f8-7c20-4d6d-8e97-4084fc26417c', 'd3d0940c-9b5c-4a70-b75c-e8a622070a7c');
