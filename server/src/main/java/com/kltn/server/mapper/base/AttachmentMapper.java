1dcb278132b5374c58756ec04aba734a829aaed1
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-06-03 22:59:34 +0700

    Fix save topic in project, fix convert response of objectId

--- server/src/main/java/com/kltn/server/mapper/base/AttachmentMapper.java
+++ server/src/main/java/com/kltn/server/mapper/base/AttachmentMapper.java
@@ -3,6 +3,7 @@ package com.kltn.server.mapper.base;
 import com.kltn.server.DTO.request.base.AttachmentRequest;
 import com.kltn.server.DTO.response.base.AttachmentResponse;
 import com.kltn.server.model.collection.model.Attachment;
+import org.bson.types.ObjectId;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
@@ -10,10 +11,13 @@ import org.mapstruct.Named;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring"
+)
 public interface AttachmentMapper {
-    @Mappings({@Mapping(target = "resourceId", source = "resourceId"),
-    })
+    @Mappings(
+            {
+                    @Mapping(target = "resourceId", source = "resourceId"),
+            })
     @Named("toDocument")
     Attachment toDocument(AttachmentRequest attachmentResponse);
 
@@ -25,5 +29,10 @@ public interface AttachmentMapper {
     AttachmentResponse toAttachmentResponse(Attachment attachment);
 
     @Named("toListResponse")
+    @Mapping(target = "id", source = "id", qualifiedByName = "objectIdToString")
     List<AttachmentResponse> toAttachmentResponseList(List<Attachment> attachments);
+    @Named("objectIdToString")
+    default String  objectIdToString(ObjectId id){
+        return id != null ? id.toHexString() : null;
+    }
 }
