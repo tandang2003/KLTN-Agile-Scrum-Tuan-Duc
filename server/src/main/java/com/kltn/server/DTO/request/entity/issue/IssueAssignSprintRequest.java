afb50afe8d140744ebe11c95b29923b6a94f30eb
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-06-06 13:46:01 +0700

    fix position type, assign/reassign sprint for issue api

--- server/src/main/java/com/kltn/server/DTO/request/entity/issue/IssueAssignSprintRequest.java
+++ server/src/main/java/com/kltn/server/DTO/request/entity/issue/IssueAssignSprintRequest.java
@@ -1,22 +1,7 @@
 package com.kltn.server.DTO.request.entity.issue;
 
 public class IssueAssignSprintRequest {
-    private String sprintId;
     private String issueId;
+    private String issueRelatedId;
 
-    public String getSprintId() {
-        return sprintId;
-    }
-
-    public void setSprintId(String sprintId) {
-        this.sprintId = sprintId;
-    }
-
-    public String getIssueId() {
-        return issueId;
-    }
-
-    public void setIssueId(String issueId) {
-        this.issueId = issueId;
-    }
 }
