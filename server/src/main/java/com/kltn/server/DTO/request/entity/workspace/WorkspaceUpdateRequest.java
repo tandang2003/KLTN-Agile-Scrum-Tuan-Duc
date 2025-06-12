312a96d12a1ccef18e9253336c2657fc1972fdc1
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-06-12 13:19:05 +0700

    remove sprint num

--- server/src/main/java/com/kltn/server/DTO/request/entity/workspace/WorkspaceUpdateRequest.java
+++ server/src/main/java/com/kltn/server/DTO/request/entity/workspace/WorkspaceUpdateRequest.java
@@ -8,14 +8,10 @@ import java.time.Instant;
 
 import static com.kltn.server.util.constant.DateFormatString.LOCAL_DATE_TIME;
 
-public record WorkspaceUpdateRequest(@Min(1) int sprintNum,
+public record WorkspaceUpdateRequest(
                                      @NotEmpty String description,
                                      @DateTimeFormat(pattern = LOCAL_DATE_TIME)
                                        Instant end) {
-    @Override
-    public int sprintNum() {
-        return sprintNum;
-    }
 
     @Override
     public String description() {
