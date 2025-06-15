7d15f30be5b50a61b8f462019be6d8ef1a982275
Le Anh Duc <ducvui2003@gmail.com>
2025-06-15 12:42:49 +0700

    feat: handle delete skill

--- client/src/components/skill/ItemSkill.tsx
+++ client/src/components/skill/ItemSkill.tsx
@@ -7,19 +7,47 @@ import {
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
 import { useAppDispatch } from '@/context/redux/hook'
+import { useDeleteSkillMutation } from '@/feature/skill/skill.api'
 import { setUpdateDataSkill } from '@/feature/skill/skill.slice'
 import { enableDialogSkill } from '@/feature/trigger/trigger.slice'
 import { SkillLevel } from '@/types/model/typeOf'
 import { SkillResponse } from '@/types/skill.type'
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
+import { toast } from 'sonner'
+
 type ItemSkillProps = {
   data: SkillResponse
 }
 
 const ItemSkill = ({ data }: ItemSkillProps) => {
   const dispatch = useAppDispatch()
-  const handleUpdate = (data: SkillResponse) => {
+  const [deleteSkill] = useDeleteSkillMutation()
+
+  const handleUpdate = () => {
     dispatch(setUpdateDataSkill(data))
     dispatch(enableDialogSkill())
+  }
+
+  const handleDelete = () => {
+    deleteSkill(data)
+      .unwrap()
+      .then(() => {
+        console.log('Skill deleted successfully')
+      })
+      .catch((error) => {
+        toast.error('Failed to delete skill')
+      })
   }
 
   return (
@@ -33,13 +61,32 @@ const ItemSkill = ({ data }: ItemSkillProps) => {
           <Icon icon={'ri:more-fill'} className='ml-3' />
         </DropdownMenuTrigger>
         <DropdownMenuContent align='end'>
-          <DropdownMenuItem
-            onClick={() => {
-              handleUpdate(data)
-            }}
-          >
-            Update
-          </DropdownMenuItem>
+          <DropdownMenuItem onClick={handleUpdate}>Update</DropdownMenuItem>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+                Delete
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Are you absolutely delete skill
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action cannot be undone. This will permanently delete
+                  your account and remove your data from our servers.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={handleDelete}>
+                  Continue
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </DropdownMenuContent>
       </DropdownMenu>
     </div>
