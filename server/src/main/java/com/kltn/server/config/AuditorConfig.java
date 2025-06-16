f77af27911bf207ac97da44fa3b2ded2acc98db1
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-06-16 08:50:08 +0700

    change get issue for backlog because have to check reopen add reopen api add get all user for admin

--- server/src/main/java/com/kltn/server/config/AuditorConfig.java
+++ server/src/main/java/com/kltn/server/config/AuditorConfig.java
@@ -2,7 +2,6 @@ package com.kltn.server.config;
 
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -11,13 +10,14 @@ import java.util.Optional;
 @Component
 public class AuditorConfig implements AuditorAware<String> {
 
-    @Override
-    public Optional<String> getCurrentAuditor() {
-        return
-                Optional.ofNullable(SecurityContextHolder.getContext())
-                        .map(SecurityContext::getAuthentication)
-                        .filter(Authentication::isAuthenticated)
-                        .map(Authentication::getPrincipal).map(String.class::cast);
+  @Override
+  public Optional<String> getCurrentAuditor() {
+    Authentication authentication = SecurityContextHolder.getContext()
+                                        .getAuthentication();
+    if (authentication != null) {
+      return Optional.of(authentication.getPrincipal().toString());
     }
+    return Optional.empty();
+  }
 
 }
--- server/src/main/java/com/kltn/server/mapper/entity/UserMapper.java
+++ server/src/main/java/com/kltn/server/mapper/entity/UserMapper.java
@@ -1,78 +1,110 @@
 package com.kltn.server.mapper.entity;
 
 import com.kltn.server.DTO.request.entity.auth.RegisterRequest;
+import com.kltn.server.DTO.request.entity.auth.TeacherRegisterRequest;
 import com.kltn.server.DTO.response.auth.AuthenticationResponse;
 import com.kltn.server.DTO.response.user.UserResponse;
+import com.kltn.server.DTO.response.user.UserResponseAll;
 import com.kltn.server.DTO.response.workspace.WorkspaceResponse;
 import com.kltn.server.model.entity.Role;
 import com.kltn.server.model.entity.User;
 import com.kltn.server.model.entity.Workspace;
+import jakarta.validation.Valid;
 import org.mapstruct.*;
 import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring",
         unmappedSourcePolicy = org.mapstruct.ReportingPolicy.IGNORE,
-        unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, uses = {WorkspaceMapper.class})
+        unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE,
+        uses = {WorkspaceMapper.class})
 public interface UserMapper {
-    User toUser(RegisterRequest registerRequest);
+  User toUser(RegisterRequest registerRequest);
 
-    default String mapRoleToString(Role role) {
-        return role != null ? role.getName() : null;
-    }
+  default String mapRoleToString(Role role) {
+    return role != null ? role.getName() : null;
+  }
 
-    @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-            @Mapping(target = "email", source = "email"),
-            @Mapping(target = "uniId", source = "uniId"),
-            @Mapping(target = "role", source = "role.name")
-    })
-    @BeanMapping(ignoreByDefault = true)
-    @Named("toUserDetailDTO")
-    AuthenticationResponse.UserDetailDTO toUserDetailDTO(User user);
+  @Mappings({@Mapping(target = "id",
+                      source = "id"),
+             @Mapping(target = "name",
+                      source = "name"),
+             @Mapping(target = "email",
+                      source = "email"),
+             @Mapping(target = "uniId",
+                      source = "uniId"),
+             @Mapping(target = "role",
+                      source = "role.name")})
+  @BeanMapping(ignoreByDefault = true)
+  @Named("toUserDetailDTO")
+  AuthenticationResponse.UserDetailDTO toUserDetailDTO(User user);
 
-    @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-            @Mapping(target = "email", source = "email"),
-            @Mapping(target = "uniId", source = "uniId"),
-            @Mapping(target = "className", source = "className"),
-            @Mapping(target = "role", source = "role.name")
-    })
-    @BeanMapping(ignoreByDefault = true)
-    UserResponse toUserResponse(User user);
+  @Mappings({@Mapping(target = "id",
+                      source = "id"),
+             @Mapping(target = "name",
+                      source = "name"),
+             @Mapping(target = "email",
+                      source = "email"),
+             @Mapping(target = "uniId",
+                      source = "uniId"),
+             @Mapping(target = "className",
+                      source = "className"),
+             @Mapping(target = "role",
+                      source = "role.name")})
+  @BeanMapping(ignoreByDefault = true)
+  UserResponse toUserResponse(User user);
 
-//FIX
-    @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-            @Mapping(target = "uniId", source = "uniId"),
-            @Mapping(target = "className", source = "className"),
-            @Mapping(target = "role", source = "role.name"),
+
+  //FIX
+  @Mappings({@Mapping(target = "id",
+                      source = "id"),
+             @Mapping(target = "name",
+                      source = "name"),
+             @Mapping(target = "uniId",
+                      source = "uniId"),
+             @Mapping(target = "className",
+                      source = "className"),
+             @Mapping(target = "role",
+                      source = "role.name"),
 //            @Mapping(target = "project", source = "project", qualifiedByName = "projectToProjectResponse"),
-    })
-    @BeanMapping(ignoreByDefault = true)
-    UserResponse toWorkspaceStudentResponse(User user);
+  })
+  @BeanMapping(ignoreByDefault = true)
+  UserResponse toWorkspaceStudentResponse(User user);
 
 
-    @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-            @Mapping(target = "email", source = "email"),
-            @Mapping(target = "uniId", source = "uniId"),
-            @Mapping(target = "className", source = "className"),
-            @Mapping(target = "workspace", source = "workspaces", qualifiedByName = "workspaceToWorkspaceResponse"),
-    })
-    @BeanMapping(ignoreByDefault = true)
-    UserResponse toUserWorkspaceResponse(User user);
+  @Mappings({@Mapping(target = "id",
+                      source = "id"),
+             @Mapping(target = "name",
+                      source = "name"),
+             @Mapping(target = "email",
+                      source = "email"),
+             @Mapping(target = "uniId",
+                      source = "uniId"),
+             @Mapping(target = "className",
+                      source = "className"),
+             @Mapping(target = "workspace",
+                      source = "workspaces",
+                      qualifiedByName = "workspaceToWorkspaceResponse"),})
+  @BeanMapping(ignoreByDefault = true)
+  UserResponse toUserWorkspaceResponse(User user);
 
 
-    @Mappings({
-            @Mapping(target = "id", source = "id"),
-            @Mapping(target = "name", source = "name"),
-    })
-    @BeanMapping(ignoreByDefault = true)
-    @Named("workspaceToWorkspaceResponse")
-    WorkspaceResponse workspaceToWorkspaceResponse(Workspace workspace);
+  @Mappings({@Mapping(target = "id",
+                      source = "id"),
+             @Mapping(target = "name",
+                      source = "name"),})
+  @BeanMapping(ignoreByDefault = true)
+  @Named("workspaceToWorkspaceResponse")
+  WorkspaceResponse workspaceToWorkspaceResponse(Workspace workspace);
 
+  User toUser(@Valid TeacherRegisterRequest registerRequest);
+
+  @Mappings({@Mapping(target = "role",
+                      source = "role.name"),
+             @Mapping(target = "createdAt",
+                      source = "dtCreated"),
+             @Mapping(target = "modifiedAt",
+                      source = "dtModified"),
+             @Mapping(target = "deletedAt",
+                      source = "dtDeleted"),})
+  UserResponseAll toUserResponseForAdmin(User user);
 }
