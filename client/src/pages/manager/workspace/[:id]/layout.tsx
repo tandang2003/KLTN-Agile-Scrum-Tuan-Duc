086b44feab6bd86c8480a9702fbde173f5fc1e2e
Lê Anh Đức <leanhduc.develop@gmail.com>
2025-06-18 18:18:50 +0700

    hard code (#160)

--- client/src/pages/manager/workspace/[:id]/layout.tsx
+++ client/src/pages/manager/workspace/[:id]/layout.tsx
@@ -1,6 +1,10 @@
 import { useAppDispatch } from '@/context/redux/hook'
+import { setSprintIdFilter } from '@/feature/board/board.slice'
 import { getTokenProjectThunk } from '@/feature/project/project.slice'
+import { setSprintCurrent } from '@/feature/sprint/sprint.slice'
+import { useGetWorkspaceQuery } from '@/feature/workspace/workspace.api'
 import { setCurrentWorkspaceId } from '@/feature/workspace/workspace.slice'
+import { toISODateString } from '@/lib/date.helper'
 import { WorkspaceParams } from '@/types/route.type'
 import { useEffect } from 'react'
 import { Outlet, useNavigate, useParams } from 'react-router-dom'
@@ -9,6 +13,9 @@ const WorkspaceDetailLayout = () => {
   const { workspaceId } = useParams<WorkspaceParams>()
   const dispatch = useAppDispatch()
   const navigate = useNavigate()
+  const { data } = useGetWorkspaceQuery(workspaceId as string, {
+    skip: !workspaceId
+  })
 
   useEffect(() => {
     if (!workspaceId) {
@@ -18,6 +25,20 @@ const WorkspaceDetailLayout = () => {
     dispatch(setCurrentWorkspaceId(workspaceId))
     dispatch(getTokenProjectThunk(workspaceId))
   }, [navigate, workspaceId, dispatch])
+
+  useEffect(() => {
+    if (data?.currentSprint) {
+      dispatch(
+        setSprintCurrent({
+          id: data.currentSprint.id,
+          start: toISODateString(data.currentSprint.start),
+          end: toISODateString(data.currentSprint.end)
+        })
+      )
+      dispatch(setSprintIdFilter(data.currentSprint.id))
+    }
+  }, [data?.currentSprint, dispatch])
+
   return (
     <Outlet
       context={{
