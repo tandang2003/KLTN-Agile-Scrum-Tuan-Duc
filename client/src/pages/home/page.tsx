712e30c90b0ebe632d8101931b13d6c48bf5015e
Le Anh Duc <ducvui2003@gmail.com>
2025-07-04 12:36:04 +0700

    add description into sprint template

--- client/src/pages/home/page.tsx
+++ client/src/pages/home/page.tsx
@@ -1,13 +1,10 @@
 import Container from '@/components/Container'
 import Header from '@/components/Header'
-import { Navigate } from 'react-router-dom'
 const HomePage = () => {
   return (
     <>
       <Header />
-      <Container>
-        <Navigate to='/auth/login' replace />
-      </Container>
+      <Container>Home page</Container>
     </>
   )
 }
