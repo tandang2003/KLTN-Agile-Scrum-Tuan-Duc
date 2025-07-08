6a994f654d3e89b3581630773fef84862f7c574f
tandang2003 <21130171@st.hcmuaf.edu.vn>
2025-07-09 00:38:14 +0700

    store data

--- server/src/main/java/com/kltn/server/repository/entity/IssueRepository.java
+++ server/src/main/java/com/kltn/server/repository/entity/IssueRepository.java
@@ -21,7 +21,6 @@ public interface IssueRepository extends JpaRepository<Issue, String> {
   @Query("SELECT i FROM Issue i WHERE i.project.id = ?1 AND i.id != ?2")
   Optional<List<Issue>> findByProjectIdAndIdNot(String projectId, String issueId);
 
-  int countByProjectIdAndSprintIdAndDtCreatedBefore(String projectId, String sprintId, Instant dtCreatedBefore);
 
   int countByProjectIdAndSprintIdAndDtCreatedAfter(String id, String id1, Instant dtStart);
 
@@ -46,4 +45,10 @@ public interface IssueRepository extends JpaRepository<Issue, String> {
   @Query(value = "SELECT ir.* FROM issue_relation ir WHERE ir.issue_id= :issueId ",
          nativeQuery = true)
   List<IssueRelation> getIssueRelation(String issueId);
+
+  int countByProjectIdAndSprintIdAndDtAppendBefore(String projectId, String sprintId, Instant dtAppendBefore);
+
+  int countByProjectIdAndSprintIdAndDtAppendLessThanEqual(String projectId, String sprintId, Instant dtAppendIsLessThan);
+
+  int countByProjectIdAndSprintIdAndDtAppendGreaterThan(String projectId, String sprintId, Instant dtAppendIsGreaterThan);
 }
