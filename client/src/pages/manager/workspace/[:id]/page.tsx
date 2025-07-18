67bd037e6aaca1ddbf036ceb039763454a44fdee
Lê Anh Đức <leanhduc.develop@gmail.com>
2025-07-18 13:57:49 +0700

    Feat/sequence (#212)
    * update sequence folder
    
    * update sequence folder
    
    * fix model
    
    * feat: update sequence
    
    * feat: response from server to client when predict
    
    * add sequence
    
    ---------
    
    Co-authored-by: tandang2003 <21130171@st.hcmuaf.edu.vn>

--- client/src/pages/manager/workspace/[:id]/page.tsx
+++ client/src/pages/manager/workspace/[:id]/page.tsx
@@ -10,7 +10,6 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
-import WorkspaceCourseCheckLayer from '@/components/workspace/WorkspaceCourseCheckLayer'
 import RequiredAuth from '@/components/wrapper/RequiredAuth'
 import messages from '@/constant/message.const'
 import { useGetWorkspaceQuery } from '@/feature/workspace/workspace.api'
