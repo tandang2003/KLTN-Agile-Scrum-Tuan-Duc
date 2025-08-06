798da3e77508ae52ef12f5c47fbe11d8be8e3e41
Le Anh Duc <ducvui2003@gmail.com>
2025-08-06 22:51:41 +0700

    fix conflict

--- client/src/providers/DateProvider.tsx
+++ client/src/providers/DateProvider.tsx
@@ -1,9 +1,5 @@
 import { createCtx } from '@/lib/context.helper'
-<<<<<<< HEAD
 import { ReactNode, useState } from 'react'
-=======
-import { ReactNode, useEffect, useState } from 'react'
->>>>>>> origin/dev
 
 type DateProviderType = {
   now: Date
@@ -14,13 +10,6 @@ const [useDate, DateProviderContext] = createCtx<DateProviderType>()
 const DateProvider = ({ children }: { children: ReactNode }) => {
   const [now, setNow] = useState<Date>(new Date())
 
-<<<<<<< HEAD
-=======
-  useEffect(() => {
-    console.log(now)
-  }, [now])
-
->>>>>>> origin/dev
   return (
     <DateProviderContext
       value={{
